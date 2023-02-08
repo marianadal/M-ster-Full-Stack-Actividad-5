cc1e85d13ec6ed685cdd9e9ca1a09142a79ad67f
Maria Nadal <nadal.miro@gmail.com>
2023-02-08 11:00:29 +0100

    Creación de componentes: header, blog y footer
    Creación de componentes: header, blog y footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BlogComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
